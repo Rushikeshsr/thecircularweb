3447f6626acb8c231b6c6a20c233d78590545f32
Rushikeshsr <Rushikeshsr@users.noreply.github.com>
2023-06-02 15:25:20 +0530

    others and home page updated

--- circular/src/components/others.js
+++ circular/src/components/others.js
@@ -5,27 +5,29 @@ const Others = () => {
   return (
     <>
         <div className='cover-imageM p-5'>
-            <h1 className='cover-text' style={{color:"black", opacity:"0.8"}}>MEDICINE</h1>
+            <h1 className='cover-text' style={{color:"black", opacity:"0.8"}}>OTHERS</h1>
         </div>
         <div className='section2 p-5'>
             <p className='paragh'>
-                We realized the vital importance of accessible healthcare services, especially during challenging times like the Covid-19 pandemic. Through strategic partnerships and innovative models, we have made significant strides in overcoming logistical barriers and streamlining operations.
+                Recognizing the vital role of healthcare, The Circular has formed partnerships with primary healthcare centers (PHCs) and community health workers. These collaborations allow us to extend our reach to underserved communities, particularly in rural and tribal regions, where access to healthcare is limited. By working closely with PHCs and community health workers, we are able to provide essential healthcare services and education, addressing the healthcare gap and empowering individuals to take charge of their well-being. Together, we strive to build healthier and more resilient communities.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Collaboration for Seamless Healthcare</span><br/>
-                We have joined hands with multiple organizations and healthcare facilities to ensure that medicines reach every individual in need. Our collaborative efforts have enabled us to establish a robust network that spans across different regions, ensuring efficient distribution of medicines. By working closely with healthcare providers, we aim to bridge the gap between supply and demand, ultimately improving the overall healthcare experience for individuals.
+                Education is a powerful catalyst for change, and The Circular has joined hands with passionate educationists, schools, and educational institutions. By pooling our resources and expertise, we are equipping young minds with the knowledge and skills they need to shape a better future. Our collaborative efforts extend to rural areas, where we work tirelessly to ensure that quality education is accessible to all, regardless of their socio-economic background. Through these partnerships, we aim to empower individuals, strengthen communities, and foster a culture of lifelong learning.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Navigating Challenges During the Pandemic</span><br/>
-                The Covid-19 pandemic presented unprecedented challenges in the healthcare sector, including disruptions in logistics and supply chains. However, we persevered and adapted to the changing circumstances. Our team worked tirelessly to find innovative solutions and implement strategic partnerships to address the emerging needs of communities. Despite the obstacles, we remained committed to our mission and continued to serve the communities in a seamless manner.
+                Our partnerships extend beyond the healthcare and education sectors, encompassing areas such as environmental conservation, renewable energy, waste management, and social entrepreneurship. By collaborating with organizations and individuals who share our vision, we are able to drive innovation and promote sustainable practices. Together, we tackle critical challenges such as climate change, resource depletion, and social inequality. Through collective action, we strive to create a positive impact that transcends boundaries and inspires others to join our cause.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Innovative Models for Accessibility</span><br/>
-                We believe in the power of innovation to enhance healthcare accessibility. Through the use of innovative models, such as mobile healthcare units, we have been able to reach remote areas and underserved communities. These models have enabled us to bring medicines closer to individuals who would otherwise face challenges in accessing them. By leveraging technology and strategic partnerships, we are paving the way for a more inclusive and accessible healthcare system.
+                One of our notable partnerships is with the <span style={{"color":"#00ff0d"}}>Japal-Rangapur observatory</span>, the first-of-its-kind in India. Through this collaboration, we are working to transform the observatory into a cutting-edge research center focused on global and planetary health. The observatory should serve as a reference point for clinicians and researchers to develop new studies related to emerging pathologies and environmental medicine. Our goal is to make sure that eventually it also acts as an incubator for new ideas and projects in the field, bringing together academic and scientific publications and information from different sectors. 
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Proud to Serve the Communities</span><br/>
-                We take immense pride in our ability to serve the communities and make a positive impact on people's lives. Our dedication to improving access to medicines and healthcare remains unwavering. We are constantly exploring new avenues, forging partnerships, and implementing innovative approaches to create a seamless and equitable healthcare ecosystem.
+                Leveraging new technologies, we aim to accelerate and optimize research results for the benefit of patients and society as a whole. Furthermore, the observatory serves as a platform to disseminate research findings, making them accessible to the broader community. In addition, we actively seek solutions for data management and address legal gaps to ensure the responsible use of data generated through research. By harnessing the power of research and collaboration, we aim to generate a positive impact on the economy and contribute to the reduction of health spending.
+            </p>
+            <p className='paragh'>
+                The Circular will become the meeting and intersection point among different players and sectors, which play a role on global and planetary health.
+            </p>
+            <p className='paragh'>
+                Through our diverse range of partnerships, The Circular remains committed to driving positive change and creating a sustainable future. By joining forces with like-minded organizations, we are able to amplify our impact and work towards a more inclusive, equitable, and environmentally conscious society. Together, we believe in the power of collaboration to create lasting change and make a difference in the lives of individuals and communities.
             </p>
         </div>
     </>
