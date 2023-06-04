ffa375749b7b7b46b44143778856e03662141e8f
Saimanishekar <saimanishekar9@gmail.com>
2023-06-04 10:31:15 +0530

    smae gallery created

---
+++ circular/src/components/smaegallery.js
@@ -0,0 +1,62 @@
+
+
+export const smaeGallery = [
+    {
+      original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si14_mvkap9.jpg",
+      thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853341/Circular%20website/smae%20gallery/si1_mxxm7p.jpg",
+    },
+    {
+      original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853341/Circular%20website/smae%20gallery/si1_mxxm7p.jpg",
+      thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si12_ch9rhl.jpg",
+    },
+    {
+      original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si12_ch9rhl.jpg",
+      thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si13_wwg7ky.jpg",
+    },
+    {
+      original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si13_wwg7ky.jpg",
+      thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si11_lzre5b.jpg",
+    },
+    {
+      original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si11_lzre5b.jpg",
+      thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si3_imqqq2.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si3_imqqq2.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si10_ln40h7.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si10_ln40h7.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si9_d0qrto.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si9_d0qrto.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si5_utndif.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si5_utndif.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si8_mbfeam.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si8_mbfeam.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si7_bwkc44.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si7_bwkc44.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si4_y8jdnm.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si4_y8jdnm.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si6_pcdkvx.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si6_pcdkvx.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si2_zl7q9a.jpg",
+    },
+    {
+        original: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853339/Circular%20website/smae%20gallery/si2_zl7q9a.jpg",
+        thumbnail: "https://res.cloudinary.com/dh4bpcja3/image/upload/v1685853340/Circular%20website/smae%20gallery/si14_mvkap9.jpg",
+    },
+    
+
+  ];
