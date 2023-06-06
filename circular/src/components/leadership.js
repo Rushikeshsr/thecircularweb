f2f9c566a6482b5fc9ae95629ff49b02854decc2
Saimanishekar <saimanishekar9@gmail.com>
2023-06-06 16:27:42 +0530

    leaderships added

--- circular/src/components/leadership.js
+++ circular/src/components/leadership.js
@@ -8,71 +8,73 @@ const Leadership = () => {
       },[]);
   return (
     <>
-    <div className='p-5'>
-        <h2 className='main-heading'>LEADERSHIP</h2>
-        <p className='lpara mb-5'>
-            At <span style={{color:"00ffod"}}>The Circular</span>, our vision is to create a sustainable and inclusive world, where communities thrive in harmony with the environment. We envision zero-disease sustainable communities, where individuals have equal opportunities, access to essential services, and a high quality of life. We aspire to build a future where human well-being and ecological balance go hand in hand, fostering a resilient planet for generations to come.
-        </p>
-        <div className='profile'>
-            <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
-                <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="aishwaryak"/>
-                <div className='lpara'>
-                    <p>Name</p>
-                    <p>Designation</p>
-                    <p>mail</p>
-                </div>                
+        <div className='p-5'>
+            <h2 className='main-heading text-center'>LEADERSHIP</h2>
+            <hr className='h-line'/>
+            <div className='profile'>
+                <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
+                    <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="skbp"/>
+                    <div className='lpara'>
+                        <p>Dr. S.K. Bikshapathy</p>
+                        <p>Chairman</p>
+                        <p>mail</p>
+                    </div>                
+                </div>
+                <p className='para'>description</p>
             </div>
-            <p className='para'>description</p>
-        </div>
-        <div className='profile'>
-            <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
-                <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="aishwaryak"/>
-                <div className='lpara'>
-                    <p>Name</p>
-                    <p>Designation</p>
-                    <p>mail</p>
-                </div>                
+            <div className='profile'>
+                <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
+                    <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1685849361/Circular%20website/AbhayCSwb_bfwjla.png" alt="abhay"/>
+                    <div className='lpara'>
+                        <p>Abhay Chakra Sadineni</p>
+                        <p>President</p>
+                        <p>mail</p>
+                    </div>                
+                </div>
+                <p className='para'>description</p>
             </div>
-            <p className='para'>description</p>
-        </div>
-        <div className='profile'>
-            <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
-                <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="aishwaryak"/>
-                <div className='lpara'>
-                    <p>Name</p>
-                    <p>Designation</p>
-                    <p>mail</p>
-                </div>                
+            <div className='profile'>
+                <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
+                    <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686048185/Circular%20website/rushi_pic_s7qs8q.jpg" alt="rushi_pic"/>
+                    <div className='lpara'>
+                        <p>Rushikeshwar Sunkey</p>
+                        <p>General Secretary</p>
+                        <p>mail</p>
+                    </div>                
+                </div>
+                <p className='para'>description</p>
             </div>
-            <p className='para'>description</p>
-        </div>
-        <div className='profile'>
-            <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
-                <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="aishwaryak"/>
-                <div className='lpara'>
-                    <p>Name</p>
-                    <p>Designation</p>
-                    <p>mail</p>
-                </div>                
+            <div className='profile'>
+                <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
+                    <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686047689/Circular%20website/passport_photo1_cm5emw.jpg" alt="saimanishekar"/>
+                    <div className='lpara'>
+                        <p>Sai Mani Shekar Sundharagiri</p>
+                        <p>Head of Digital Transformation</p>
+                        <p>mail</p>
+                    </div>                
+                </div>
+                <p className='para'>description</p>
             </div>
-            <p className='para'>description</p>
-        </div>
-        <div className='profile'>
-            <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
-                <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686030527/Circular%20website/skbp_pic_k9yxuw.jpg" alt="aishwaryak"/>
-                <div className='lpara'>
-                    <p>Name</p>
-                    <p>Designation</p>
-                    <p>mail</p>
-                </div>                
+            <div className='profile'>
+                <div data-aos="flip-right" data-aos-delay="100" className='lbox-image'>
+                    <img className='lperson' src="https://res.cloudinary.com/dh4bpcja3/image/upload/v1686048337/Circular%20website/Adi_Pic_xt0iiz.jpg" alt="adi_pic"/>
+                    <div className='lpara'>
+                        <p>Adi Sheshan Mula</p>
+                        <p>Head of Supply Chain Management</p>
+                        <p>mail</p>
+                    </div>                
+                </div>
+                <p className='para'>description</p>
             </div>
-            <p className='para'>description</p>
-        </div>
 
-       
+        
 
 
-    </div>
+        </div>
+        <div className='section4 p-5'>
+            <h2 className='main-heading text-center'>ADVISORS</h2>
+            <hr className='h-line' style={{borderColor:"aliceblue", opacity:"1"}}/>
+        </div>
     </>
   )
 }
