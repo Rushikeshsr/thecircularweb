f9def307491734e656e259a6aa239b18fec859b0
Rushikeshsr <Rushikeshsr@users.noreply.github.com>
2023-06-06 10:44:14 +0530

    operations content updated

--- circular/src/components/operations.js
+++ circular/src/components/operations.js
@@ -6,7 +6,7 @@ const Operations = () => {
     <div className='p-5'>
         <h2 className='main-heading'>OPERATIONS</h2>
         <p className='para'>
-        Operations and Implementation play a vital role in translating ideas and strategies into tangible outcomes and real-world impact. They are the driving force behind turning our vision into reality.
+            Operations and Implementation play a vital role in translating ideas and strategies into tangible outcomes and real-world impact. They are the driving force behind turning our vision into reality.
         </p>
         <p className='para'>Our Operations Team</p>
         <div className='d-flex justify-content-center'>
@@ -123,10 +123,33 @@ const Operations = () => {
                     <td>Johnny Vemagiri</td>
                     <td>SA70810</td>
                 </tr>
-                
             </table>
         </div>
-        
+        <p className='para mt-3'>
+            The Circular recognizes the importance of collaboration with government bodies to make a significant impact on environmental sustainability. In Telangana, the state government has taken commendable initiatives to save the environment and promote a circular economy. One such initiative is the Rethink - Reduce, Reuse, and Recycle (RRR) Knowledge Hub, introduced in partnership with ASCI (Administrative Staff College of India).
+        </p>
+        <p className='para'>
+            The RRR Knowledge Hub aims to provide a platform for knowledge sharing, research, and training in waste management practices. By promoting the principles of reducing, reusing, and recycling, the government is taking proactive steps to minimize waste generation and promote sustainable practices across various sectors. In addition to the RRR Knowledge Hub, the Telangana government, in collaboration with ASCI, has launched the Environmental Surveillance Laboratory. This laboratory will play a crucial role in monitoring and assessing environmental parameters, ensuring compliance with environmental regulations, and promoting sustainable development in the state. Furthermore, the government has taken ambitious steps to set up additional waste-to-energy (WTE) plants in Hyderabad and its surrounding areas. With the aim of generating 101 MW of power by December 2024, these plants will convert refuse-derived fuel (RDF) into energy, making Hyderabad a leader in WTE conversion in the country. The plans include the establishment of WTE plants in Dindigul, Jawaharnagar Phase 2, Pyaranagar, Yacharam, and Bibinagar, with each plant contributing to the circular economy by utilizing solid waste as a valuable resource.
+        </p>
+        <p className='para'>
+            By implementing these initiatives, the Telangana government, in collaboration with other stakeholders, is creating a sustainable ecosystem where waste is managed effectively, resources are conserved, and renewable energy is harnessed. This comprehensive approach not only addresses environmental concerns but also establishes a model for other urban local bodies and gram panchayats to adopt similar practices, fostering a culture of sustainability and circular economy throughout the region. The Circular acknowledges and appreciates the proactive efforts taken by the Greater Hyderabad Municipal Corporation (GHMC) and the Telangana government in promoting sustainable waste management practices and creating a circular economy. The establishment of RRR (Reduce, Reuse, and Recycle) centers in each of the 150 wards of GHMC is a commendable step towards decentralized waste management.
+        </p>
+        <p className='para'>
+            With 1,962 operational RRR centers across 142 Urban Local Bodies (ULBs) in the state, over 200 metric tonnes of materials such as plastic goods, books, clothes, and footwear have been collected since May 20th, and these will be refurbished, reused, or remade into new products. This not only prevents waste accumulation but also contributes to the reduction of resource consumption and environmental impact. Hyderabad alone collects approximately 190 tons of recyclables daily, demonstrating the city's commitment to sustainable waste management. The presence of 206 dry resource collection centers, with 87 managed by women's self-help groups (SHGs), not only generates revenue but also empowers local communities and promotes inclusive development.
+        </p>
+        <p className='para'>
+            The Telangana government's initiatives extend beyond solid waste management. The upcoming construction and demolition (C&D) waste processing plants in Nalgonda, Sangareddy, and Warangal will ensure the efficient handling and recycling of construction waste, reducing its impact on the environment. Additionally, the utilization of plastic waste for the production of pavement tiles and bricks in Suryapet and Siddipet ULBs demonstrates the innovative use of materials and promotes the circular economy. Addressing organic waste management, bio-methanation plants in Greater Hyderabad Municipal Corporation, Greater Warangal Municipal Corporation, Siddipet, and Siricilla ULBs play a vital role in converting organic waste into biogas, which can be used for cooking purposes. This not only reduces the dependence on traditional energy sources but also mitigates the environmental impact of organic waste disposal. Furthermore, the commitment to treating 100 percent of sewage in Hyderabad sets a remarkable precedent. With 20 existing sewerage treatment plants (STPs) treating 714.3 million liters per day (MLD) of wastewater and an additional 31 STPs with a capacity to treat 1,259.5 MLD in the pipeline, Hyderabad is poised to become a leader in sewage treatment in the country. The government's approval for setting up rooftop solar power plants at STPs, pump houses, reservoirs, and buildings of the Hyderabad Metro Water Supply and Sewerage Board (HMWSSB) further emphasizes the integration of renewable energy and sustainable practices into the city's infrastructure.
+        </p>
+        <p className='para'>
+            These initiatives collectively demonstrate the Telangana government's commitment to environmental sustainability, resource optimization, and the establishment of a circular economy.  We recognize the invaluable role played by self-help groups (SHGs) in promoting sustainability and empowering local communities. We believe in the power of collaboration and community involvement to drive positive change. Therefore, we actively partner with SHGs from the municipality wards to facilitate recycling and upcycling initiatives.
+        </p>
+        <p className='para'>
+            By collaborating with SHGs, we aim to harness their expertise, skills, and entrepreneurial spirit in waste management and recycling. These groups have shown immense potential in creating innovative products through upcycling, refurbishing, and repurposing waste materials. They contribute not only to environmental conservation but also to the economic growth of their communities. Through our partnership, we provide training, capacity building, and technical support to SHGs, enabling them to enhance their knowledge and skills in waste management practices. We assist them in accessing necessary resources, infrastructure, and market linkages, thereby creating a sustainable ecosystem for their initiatives. Our collaboration with SHGs also extends to raising awareness and promoting responsible waste disposal practices within the community. We conduct workshops, awareness campaigns, and educational programs to engage residents and encourage their active participation in recycling and upcycling efforts.
+        </p>
+        <p className='para'>
+            The Circular believes that by partnering with SHGs, we can foster a sense of ownership, inclusivity, and dignity among the workforce. Through this collaboration, we not only contribute to the circular economy but also support the economic empowerment and social well-being of the community. Together with self-help groups, we are determined to create a sustainable future where waste is seen as a valuable resource, communities thrive, and the environment is preserved for generations to come.
+        </p>
+
     </div>
     </>
   )
