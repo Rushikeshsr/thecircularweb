0a3c916737c12f37298cb2f59677d3a34a384caa
Saimanishekar <saimanishekar9@gmail.com>
2023-06-06 09:56:52 +0530

    transparency updated

--- circular/src/components/transparency.js
+++ circular/src/components/transparency.js
@@ -26,6 +26,16 @@ const Transparency = () => {
                 <span style={{fontSize:"20px",color:"#00ff0d",textShadow:"-0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px -0.5px 0 #000"}}>Curbing Student Exploitation</span><br/>
                 In the realm of education, we are committed to preventing student exploitation and unethical practices. We collaborate with educational institutions to provide a safe and transparent environment for students. Through due diligence, we assess compliance with relevant regulations and identify any risks or red flags. We prioritize the protection of whistleblowers, enabling them to report any suspected or actual corruption cases without fear of retaliation. Our enforcement of sanctions for breaches or violations of our anti-corruption program ensures the integrity and reputation of The Circular.
             </p>
+        </div>
+        <div className='section2 p-5'>
+            <div className='text-center m-3'>
+                <img className='medpich m-1' data-aos="fade-left" src={"https://res.cloudinary.com/dh4bpcja3/image/upload/v1686024346/Circular%20website/trans3_qecc3m.jpg"} alt='cloudinery trans3'></img>
+                <img className='medpich m-1' data-aos="fade-right"  src={"https://res.cloudinary.com/dh4bpcja3/image/upload/v1686024346/Circular%20website/trans4_aplvpk.jpg"} alt='cloudinery trans4'></img>
+                <img className='medpich m-2' data-aos="fade-left" style={{width:"400px",height:"400px"}}  src={"https://res.cloudinary.com/dh4bpcja3/image/upload/v1686024346/Circular%20website/trans2_xp4ekl.jpg"} alt='cloudinery tans2'></img>
+                <img className='medpich m-2' data-aos="fade-right" style={{width:"400px",height:"400px"}} src={"https://res.cloudinary.com/dh4bpcja3/image/upload/v1686025345/Circular%20website/trans1_pe3ivt.jpg"} alt='cloudinery tans1'></img>
+            </div>
+        </div>
+        <div className='p-5'>    
             <p data-aos="fade-right" data-aos-delay="200" className='para'>
                 <span style={{fontSize:"20px",color:"#00ff0d",textShadow:"-0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px -0.5px 0 #000"}}>Early Interventions for Quality Human Capital</span><br/>
                 We recognize that building quality human capital starts at an early stage. Therefore, we focus on basic and early interventions to enhance educational outcomes. We support early childhood education initiatives and remedial education programs to ensure a strong foundation for learning. Our commitment to teacher training and development empowers educators with the necessary skills to deliver quality education. We enhance educational curricula to align with the evolving needs of the workforce, integrating essential skills into the learning experience.<br/>
