52a30ab64e5fd347f5219c5ba04ee5f5f797bbcc
Rushikeshsr <Rushikeshsr@users.noreply.github.com>
2023-06-06 10:30:24 +0530

    operations table updated

--- circular/src/components/operations.js
+++ circular/src/components/operations.js
@@ -20,41 +20,110 @@ const Operations = () => {
                     <td>DL45358</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Avinash</td>
+                    <td>DL51472</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Narendra Boddupally</td>
+                    <td>DL34969</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Kiran Ch</td>
+                    <td>SA78817</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Pavan Kumar Jaligari</td>
+                    <td>DL86242</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>James</td>
+                    <td>SA40907</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Naresh Janagam</td>
+                    <td>DL80651</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Jeeva Yammala</td>
+                    <td>DL20070</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Kaibalya Sahoo</td>
+                    <td>DL25446</td>
                 </tr>
                 <tr>
-                    <td>Ashok Kumar</td>
-                    <td>DL45358</td>
+                    <td>Kalavathi</td>
+                    <td>CA17646</td>
                 </tr>
+                <tr>
+                    <td>Sridhar Kandipati</td>
+                    <td>DL21203</td>
+                </tr>
+                <tr>
+                    <td>Krishna Jogu</td>
+                    <td>DL50725</td>
+                </tr>
+                <tr>
+                    <td>Ramesh Kummari</td>
+                    <td>SA93132</td>
+                </tr>
+                <tr>
+                    <td>Mallesh</td>
+                    <td>SA45693</td>
+                </tr>
+                <tr>
+                    <td>Manideep</td>
+                    <td>SA11280</td>
+                </tr>
+                <tr>
+                    <td>Mohammed Awais</td>
+                    <td>DL53427</td>
+                </tr>
+                <tr>
+                    <td>Narsingh Mallekedi</td>
+                    <td>DL30790</td>
+                </tr>
+                <tr>
+                    <td>Padmaja</td>
+                    <td>CA13151</td>
+                </tr>
+                 <tr>
+                    <td>Rajel Ahmed</td>
+                    <td>DL87254</td>
+                </tr>
+                <tr>
+                    <td>Raja Gudivaka</td>
+                    <td>AS93999</td>
+                </tr>
+                <tr>
+                    <td>Rakesh</td>
+                    <td>DL34884</td>
+                </tr>
+                <tr>
+                    <td>Ram Prakash</td>
+                    <td>DL44845</td>
+                </tr>
+                <tr>
+                    <td>Ramreddy</td>
+                    <td>DL59614</td>
+                </tr>
+                <tr>
+                    <td>Shivangi</td>
+                    <td>AL30866</td>
+                </tr>
+                <tr>
+                    <td>Srikanth</td>
+                    <td>AS17948</td>
+                </tr>
+                <tr>
+                    <td>Sudhakar</td>
+                    <td>DL77907</td>
+                </tr>
+                <tr>
+                    <td>Johnny Vemagiri</td>
+                    <td>SA70810</td>
+                </tr>
+                
             </table>
         </div>
         
