fb46d8abe1068053ff6ff892c761b7781f250188
Saimanishekar <saimanishekar9@gmail.com>
2023-06-17 13:45:12 +0530

    hosipitality page resp done

--- circular/src/components/hospitality.js
+++ circular/src/components/hospitality.js
@@ -10,27 +10,27 @@ const Hospitality = () => {
         <div className='cover-imageH p-5'>
             <h1 className='cover-text' style={{color:"aliceblue", opacity:"0.8",textShadow:"2px 2px 20px black"}}>Hospitality</h1>
         </div>
-        <div className='section2 p-5'>
+        <div className='section2'>
             <p className='paragh'>
                 The hospitality industry should be a beacon of warmth, care, and sustainability. We understand the challenges that guests face, particularly during uncertain times like the Covid-19 pandemic, and we are committed to transforming the hospitality landscape into one that prioritizes empathy, sustainability, and exceptional experiences for all. Our journey began with a deep-rooted passion for healthcare and a vision to bridge the gap between hospitality and well-being. We recognized the pressing need for compassionate and sustainable solutions within the hospitality industry, particularly when it comes to accommodating individuals seeking medical treatments or experiencing health challenges. This realization sparked our mission to revolutionize the concept of hospitality and create a nurturing environment for all.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Why We Started</span><br/>
+                <span className='font-head' style={{fontSize:"20px",color:"#00ff0d"}}>Why We Started</span><br/>
                 We witnessed firsthand the struggles faced by patients and their families when seeking appropriate accommodation during medical journeys. The lack of empathy, hygiene, and suitable facilities within the hospitality sector left a void that needed to be filled. We were driven by a shared commitment to address these issues and provide a safe haven for individuals seeking respite and support during their healthcare journeys.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>The Circular's Impact</span><br/>
+                <span className='font-head' style={{fontSize:"20px",color:"#00ff0d"}}>The Circular's Impact</span><br/>
                 Through our unwavering dedication, we have transformed the landscape of hospitality for healthcare. We have collaborated with esteemed medical institutions and practitioners to understand the unique needs of patients, ensuring that our services are tailored to provide the utmost comfort, care, and convenience.
             </p>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Patient-Centric Approach</span><br/>
+                <span className='font-head' style={{fontSize:"20px",color:"#00ff0d"}}>Patient-Centric Approach</span><br/>
                 At The Circular, our guests' well-being is our top priority. We have carefully designed our facilities to cater to the specific requirements of patients, ensuring comfort and accessibility. From well-appointed rooms equipped with medical-grade amenities to round-the-clock assistance from our compassionate staff, we strive to create a healing environment that goes beyond conventional hospitality.
             </p>
             <div className='text-center m-3'>
                 <img className='medpich' style={{height:"350px"}} src={"https://res.cloudinary.com/dh4bpcja3/image/upload/v1685849361/Circular%20website/abidi_pvrbnx.jpg"} alt='abidi'></img>
             </div>
             <p className='paragh'>
-                <span style={{fontSize:"20px",color:"#00ff0d"}}>Safe Hospitality Management (SHM)</span> &nbsp;
+                <span className='font-head' style={{fontSize:"20px",color:"#00ff0d"}}>Safe Hospitality Management (SHM)</span> &nbsp;
                 has been a valuable partner in our mission to provide quality services in the hospitality domain, particularly in the context of healthcare. Led by Mr. Abidi, the founder of SHM, this collaboration has enabled us to establish strong connections with hospitals and ensure the well-being of patients residing in our partner hotels.
             </p>
             <p className='paragh'>
@@ -39,12 +39,14 @@ const Hospitality = () => {
             <p className='paragh'>
                 Through our collaboration with SHM, we have been able to establish a seamless system of care and support for guests seeking medical treatments or experiencing health challenges. Our partnership ensures that the hotels under SHM management are equipped with the necessary facilities, trained staff, and compassionate services to provide a comfortable and nurturing environment for patients and their loved ones. We deeply appreciate Mr. Abidi's dedication and the collective efforts of SHM in enhancing the guest experience and promoting well-being within the hospitality industry. Together, we strive to create a new standard of care and hospitality that places the needs of patients at the forefront. As we continue on our mission to redefine hospitality, we are grateful for the collaboration with SHM and the positive impact it has had on the lives of countless individuals seeking medical care and support.
             </p>
-            <div className="image-gallery-wrapper">
-                <ImageGallery autoPlay={true} showThumbnails={false} items={hospitalityGallery} />
+            <div className='para'>
+                <div className="image-gallery-wrapper">
+                    <ImageGallery autoPlay={true} showThumbnails={false} items={hospitalityGallery} />
+                </div>
             </div>
-           </div>
-           <div className='p-5'> 
             
+           </div>
+           <div className='section3'>  
             <p className='para'>
                 We understand the critical role that quality food plays in providing a holistic hospitality experience. We recognize the challenges present in the Indian restaurant industry, where many establishments follow unhealthy practices in cooking and serving food, compromising the well-being of guests. This alarming situation prompted us to take action and find innovative solutions to ensure that our guests receive nutritious and hygienic meals during their stay.
             </p>
@@ -57,8 +59,10 @@ const Hospitality = () => {
             <p className='para'>
                 Driven by our commitment to providing exceptional service and removing uncertainty in the food industry, we embarked on a journey to develop new models that prioritize quality, transparency, and integrity. We firmly believe that technology plays a vital role in bringing about transformative change at scale. By leveraging innovative technological solutions, we aim to revolutionize the way food is sourced, prepared, and served, ensuring the highest standards of hygiene, nutrition, and customer satisfaction.
             </p>
-            <div className="image-gallery-wrapper text-center">
-                <ImageGallery autoPlay={true} showThumbnails={false} items={hospitalityGallery2} />
+            <div className='para'>
+                <div className="image-gallery-wrapper text-center">
+                    <ImageGallery autoPlay={true} showThumbnails={false} items={hospitalityGallery2} />
+                </div>
             </div>
             <p className='para mt-5'>
                 Our dedicated team of experts is working tirelessly to establish strategic partnerships with responsible and ethical food suppliers, implement robust quality control measures, and employ cutting-edge technologies to streamline operations and enhance transparency. Through these efforts, we aspire to set a new benchmark for the industry, fostering a culture of excellence and accountability in the food services we provide.
@@ -67,9 +71,8 @@ const Hospitality = () => {
                 At The Circular, we remain committed to our mission of revolutionizing the hospitality industry. We are determined to overcome the challenges posed by the prevailing practices in the food sector and introduce sustainable and ethical models that prioritize the health and well-being of our guests. Together with our trusted partners, we are shaping a future where quality food is not just a luxury but a fundamental aspect of a truly exceptional hospitality experience.
             </p>
             <div className='para text-center'>
-                <button className='btn btn-danger'><a href="/resources" style={{color:"aliceblue", textDecoration:"none"}}>Check out our Operations Data</a></button> 
-            </div>
-            
+                <button className='btn btn-danger'><a href="/aboutus/impact" style={{color:"aliceblue", textDecoration:"none"}}>Check out our Operations Data</a></button> 
+            </div>    
         </div>
     </>
   )
